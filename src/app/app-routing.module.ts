671f0ef40f2c109157de67c75d0582a20a403250
Rijaa\Shams <rijaa04@gmail.com>
2018-06-01 00:51:27 +0500

    Through tortoiseGit
    Signed-off-by: Rijaa\Shams <rijaa04@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/index';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/index';
 import { AuthGuard } from './_guards/index';
  
@@ -10,6 +11,7 @@ const routes: Routes = [
   //{ path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: LoginComponent },
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
  
     // otherwise redirect to home
